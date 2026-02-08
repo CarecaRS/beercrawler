7e291e450d56893570ac7a7e3d69e5989df059ec
Thiago Werminghoff <twerminghoff@proton.me>
2026-02-08 16:27:55 +0000

    Update

---
+++ beers.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+urls = ['https://www.continente.pt/bebidas-e-garrafeira/cervejas-e-sidras/cerveja-tradicional/', 
+        'https://www.continente.pt/bebidas-e-garrafeira/cervejas-e-sidras/cerveja-estrangeira-e-artesanal/',
+        'https://www.pingodoce.pt/on/demandware.store/Sites-pingo-doce-Site/default/Search-Show?cgid=ec_cervejasnacionais_1500_100']
+
+cervejas = ['coruja india pale ale', 'guinness', 'franziskaner', 'estrela damm', 'erdinger', 'benediktiner']
+
